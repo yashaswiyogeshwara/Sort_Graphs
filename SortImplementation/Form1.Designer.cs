29dee75f3dba4b9e88d7c81b71cf5c5bcbee62ff
Yashaswi Yogeshwara <yashaswi.yogeshwara@gmail.com>
2017-09-22 11:48:24 +0530

    Basic verison of merge sort using tasks

---
+++ SortImplementation/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace SortImplementation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.DataSource = this.chart1.Images;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(112, 143);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "sorting1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(434, 318);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Customize += new System.EventHandler(this.chart1_Customize);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            this.chart1.Layout += new System.Windows.Forms.LayoutEventHandler(this.chart1_Layout);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(147, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Records";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.MouseHover += new System.EventHandler(this.textBox1_MouseHover);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Test Info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 512);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ SortImplementation/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SortImplementation
+{
+    public partial class Form1 : Form
+    {
+        private SynchronizationContext _uiSyncContext;
+        public Form1()
+        {
+            _uiSyncContext = SynchronizationContext.Current;
+            InitializeComponent();
+        }
+        private System.Timers.Timer aTimer;
+
+        private int recordIndex = 0;
+
+        private int[] GetUnsortedNumbers()
+        {
+            string[] inputArr = this.textBox1.Text.Split(',');
+            int[] unsortedRecords = new int[inputArr.Length];
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                int.TryParse(inputArr[i], out unsortedRecords[i]);
+            }
+            return unsortedRecords;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            MergeSort mergeSort = new MergeSort(this.chart1);
+
+            recordIndex = 0;
+            int[] unsortedArr = GetUnsortedNumbers().Length > 0 ? GetUnsortedNumbers() : new int[] { 7, 8, 5, 4, 12, 11, 14, 35, 2, 1, 5, 6, 8, 2, 3, 19, 10, 89, 87, 76, 56, 67, 57, 97, 67, 68, 79, 97, 89, 20, 10, 44, 55, 67, 87 };
+            this.chart1.Series["sorting1"].Points.DataBindY(unsortedArr);
+            int[] sortedArr = mergeSort.sort(unsortedArr);
+            List<List<int>> Iterations = mergeSort.GetIterations();
+
+            aTimer = new System.Timers.Timer(10000);
+
+            // Hook up the Elapsed event for the timer.
+            aTimer.Elapsed += (send, el) => OnTimedEvent(send, el, Iterations);
+
+            // Set the Interval to 2 seconds (2000 milliseconds).
+            aTimer.Interval = 2000;
+            aTimer.Enabled = true;
+        }
+
+        // to be done
+        private void OnTimedEvent(object source, ElapsedEventArgs e, List<List<int>> recordList)
+        {
+            if (recordIndex < recordList.Count)
+            {
+                // this step is to make sure that the recordIndex value is created every time a new thread is created.
+                int value = recordIndex;
+                _uiSyncContext.Post(_ => this.chart1.Series["sorting1"].Points.DataBindY(recordList[value]), null);
+                recordIndex++;
+            }
+            else
+            {
+                aTimer.Stop();
+                return;
+            }
+
+        }
+
+        private List<int> SortNumbers()
+        {
+            return null;
+        }
+        internal class MergeSort
+        {
+
+            private int _elemCount = 0;
+            private int[] _sortedArr;
+            private bool _doSort = true;
+            public Chart _chart;
+            private List<List<int>> _iterations = new List<List<int>>();
+            public MergeSort(Chart chart)
+            {
+                _chart = chart;
+                _chart.Refresh();
+            }
+
+            public List<List<int>> GetIterations()
+            {
+                return _iterations;
+            }
+
+            public int[] sort(int[] numbers)
+            {
+                _elemCount = numbers.Length;
+                List<int[]> unsortedArr = new List<int[]>();
+
+                foreach (int i in numbers)
+                {
+                    unsortedArr.Add(new int[] { i });
+                }
+                GetSortedArrList(unsortedArr);
+                return _sortedArr;
+            }
+
+            private async void GetSortedArrList(List<int[]> arrList)
+            {
+                if (arrList[0].Length == _elemCount)
+                {
+                    _doSort = false;
+                    _sortedArr = arrList[0];
+                }
+                if (_doSort)
+                {
+                    List<int[]> sortedArr = new List<int[]>();
+                    int j = 0;
+                    int i = 0;
+                    for (; i < arrList.Count - 1; i = i + 2)
+                    {
+                        int temp = i;
+                        int[] tempArr= await Task.Run(() => merge(arrList[temp], arrList[temp + 1]));
+                        sortedArr.Add(tempArr);
+                    }
+                    if (arrList.Count % 2 > 0)
+                    {
+                        sortedArr.Add(arrList[i]);
+                        i = 0;
+                    }
+                    LoadChartData(sortedArr);
+                    GetSortedArrList(sortedArr);
+                }
+
+            }
+            // merge not working
+
+            public void LoadChartData(List<int[]> arr)
+            {
+                List<int> yNum = new List<int>();
+                foreach (int[] intArr in arr)
+                {
+                    foreach (int i in intArr)
+                    {
+                        yNum.Add(i);
+                    }
+                }
+                _iterations.Add(yNum);
+            }
+
+            private int[] merge(int[] leftArr, int[] rightArr)
+            {
+               
+                    int[] arr = new int[leftArr.Length + rightArr.Length];
+                    SortArr(leftArr, rightArr, ref arr, 0);
+                    return arr;
+                
+            }
+
+            private void SortArr(int[] leftArr, int[] rightArr, ref int[] arr, int k)
+            {
+
+                if (leftArr.Length == 0)
+                {
+                    foreach (int i in rightArr)
+                    {
+                        arr[k] = i;
+                        k++;
+                    }
+                    return;
+                }
+
+                if (rightArr.Length == 0)
+                {
+                    foreach (int i in leftArr)
+                    {
+                        arr[k] = i;
+                        k++;
+                    }
+                    return;
+                }
+
+                if (leftArr[0] < rightArr[0])
+                {
+                    arr[k] = leftArr[0];
+                    leftArr = leftArr.Skip(1).ToArray();
+
+                }
+                else
+                {
+                    arr[k] = rightArr[0];
+                    rightArr = rightArr.Skip(1).ToArray();
+                }
+                k++;
+                SortArr(leftArr, rightArr, ref arr, k);
+            }
+        }
+
+        private void chart1_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void chart1_Customize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_MouseHover(object sender, EventArgs e)
+        {
+            this.toolTip1.ToolTipTitle = "Info";
+            this.toolTip1.Show("Enter numbers to be sortetd in comma seperated format", this.textBox1);
+        }
+    }
+}
